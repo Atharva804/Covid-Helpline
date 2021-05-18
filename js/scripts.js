626c6b33a80b5ced97f4f0e96da07381574b5456
Atharva804 <74488182+Atharva804@users.noreply.github.com>
2021-05-18 20:46:40 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 function otp_gen () {
 document.getElementById('mobile-no').placeholder = "OTP";
-document.getElementById('btn-send').value = "Verify";
+document.getElementById('btn-send').value = "Verify!";
+document.getElementById('mobile-num').innerHTML = "Enter OTP"
+document.getElementById('msgOTP').innerHTML = "OTP has been send to your number"
 }
